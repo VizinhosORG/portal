875133f916d7a14582d6e29013464d09c7f1d637
VizinhosORG <110699899+VizinhosORG@users.noreply.github.com>
2022-08-06 01:38:54 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c6a0ce3a06e14621b3fb2390bd105fb5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Vizinhos em Lisboa',
+  domain: 'vizinhos.org',
+  author: 'Vizinhos em Liboa - Associação de Moradores',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Uma nova geração de cidadania. Participe. Junte-se aos seus Vizinhos!',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'VizinhosORG',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -41,24 +41,32 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+    '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  }
+  //pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'A Associação',
+      pageId: '6d3790fa1da644188e936fcedce66068'
+    },
+    {
+      title: 'Núcleos Locais',
+      pageId: '502555b65b0947c09921ec79ed9aff11'
+    },
+    {
+      title: 'Iniciativas',
+      pageId: '51ceb81cab5a407b8d9a53e601ef7c0d'
+    },
+    {
+      title: 'Contactos',
+      pageId: '088587a345564104afc3e4e9d621643a'
+    }
+  ]
 })
