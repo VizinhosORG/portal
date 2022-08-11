ec2c519c8fdd52eb2c9ae5eb8ce95340079b93bc
VizinhosORG <110699899+VizinhosORG@users.noreply.github.com>
2022-08-11 02:06:08 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>© 2022 - {config.author}</div>
+      <div className={styles.copyright}>© 2022 | {config.author} | Todos os direitos reservados.</div>
 
       <div className={styles.settings}>
         {hasMounted && (
